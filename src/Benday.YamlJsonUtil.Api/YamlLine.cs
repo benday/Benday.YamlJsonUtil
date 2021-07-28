c4fdb022f07d54fd587052be09a156082f5bae4e
benday <benday@benday.com>
2021-07-28 17:06:50 -0400

    Implemented basic line parsing

--- src/Benday.YamlJsonUtil.Api/YamlLine.cs
+++ src/Benday.YamlJsonUtil.Api/YamlLine.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Benday.YamlJsonUtil.Api
 {
     public class YamlLine
@@ -6,10 +8,27 @@ namespace Benday.YamlJsonUtil.Api
         public YamlLine(string line)
         {
             _line = line;
+
+            Populate();
+        }
+
+        private void Populate()
+        {
+            var tokens = _line.Split(":");
+
+            if (tokens.Length == 1)
+            {
+                PropertyName = tokens[0].Trim();
+            }
+            else if (tokens.Length == 2)
+            {
+                PropertyName = tokens[0].Trim();
+                PropertyValue = tokens[1].Trim();
+            }
         }
 
         public string PropertyName { get; set; }
         public string PropertyValue { get; set; }
-        
+
     }
 }
